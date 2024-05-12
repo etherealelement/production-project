4f69c76438c38ba11a2162a6bd7e797505747c49
etherealelement <vitalik.safonov@list.ru>
2024-05-12 09:19:12 +0300

    feat: add classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { ThemeContext } from "./theme/ThemeContext";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export enum Theme {
   LIGHT = "light",
@@ -15,7 +16,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>toggle theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
