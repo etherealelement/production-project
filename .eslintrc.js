3f579003e1ef96d81dc8ba9a301a22af4609360c
etherealelement <vitalik.safonov@list.ru>
2024-05-19 21:06:14 +0300

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "off",
     "no-underscore-dangle": "off",
+    "i18next/no-literal-string": ["error", { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
