0384eaa48548f84174a964c2082ffd4e77dfe195
etherealelement <vitalik.safonov@list.ru>
2024-05-22 05:13:49 +0300

    feat: page-loader

--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -1,15 +1,16 @@
-import {FC} from "react";
-import "./PageLoader.scss";
-import {classNames} from "shared/lib/classNames/classNames";
+import { FC } from "react";
+import { Loader } from "shared/ui/Loader";
+import { classNames } from "shared/lib/classNames/classNames";
+import styles from "./PageLoader.module.scss";
 
 interface PageLoaderProps {
   className?: string;
-};
+}
 
 export const PageLoader: FC<PageLoaderProps> = () => {
   return (
-    <div className={classNames("page-loader", {}, [])}>
-      <span className="loader"></span>
+    <div className={classNames(styles.PageLoader, {}, [])}>
+      <Loader></Loader>
     </div>
   );
 };
